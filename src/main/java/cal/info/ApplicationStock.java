f07d3787047cc69efa6ddad0d534df79dce464f4
AmineM <mezheriamine@gmail.com>
2025-09-23 13:23:08 -0400

    application du kiss and dry sur la classe applicationstock

--- src/main/java/cal/info/ApplicationStock.java
+++ src/main/java/cal/info/ApplicationStock.java
@@ -3,21 +3,11 @@ package cal.info;
 public class ApplicationStock {
     public static void main(String[] args) {
         GestionnaireStock gestionnaire = new GestionnaireStock();
-        Produit laptop = new Produit("Laptop", 5);
-        Produit smartphone = new Produit("Smartphone", 10);
-
-        gestionnaire.ajouterProduit(laptop);
-        gestionnaire.ajouterProduit(laptop);
-        gestionnaire.ajouterProduit(smartphone);
-
+        gestionnaire.ajouterProduit(new Produit("Laptop", 5));
+        gestionnaire.ajouterProduit(new Produit("Laptop", 5));
+        gestionnaire.ajouterProduit(new Produit("Smartphone", 10));
         gestionnaire.afficherProduitsEnStock();
-
-        gestionnaire.ajouterProduit(getProduitInutilise());
+        gestionnaire.ajouterProduit(new Produit("Tablette", 0));
         gestionnaire.afficherProduitsEnStock();
     }
-
-    public static Produit getProduitInutilise() {
-        return new Produit("Tablette", 0);
-    }
-
 }
