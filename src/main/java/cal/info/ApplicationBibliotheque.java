814b69aa738e68a7f3c573e93b22dc4c112fccb6
AmineM <mezheriamine@gmail.com>
2025-09-23 13:18:18 -0400

    application du kiss and dry sur la classe applicationbibliotheque

--- src/main/java/cal/info/ApplicationBibliotheque.java
+++ src/main/java/cal/info/ApplicationBibliotheque.java
@@ -4,11 +4,8 @@ public class ApplicationBibliotheque {
     public static void main(String[] args) {
         GestionnaireBibliotheque gestionnaire = new GestionnaireBibliotheque();
 
-        Livre livre1 = new Livre("1984", "George Orwell");
-        gestionnaire.ajouterLivre(livre1);
-
-        Livre livre2 = new Livre("Le Petit Prince", "Antoine de Saint-Exupéry");
-        gestionnaire.ajouterLivre(livre2);
+        gestionnaire.ajouterLivre(new Livre("1984", "George Orwell"));
+        gestionnaire.ajouterLivre(new Livre("Le Petit Prince", "Antoine de Saint-Exupéry"));
 
         gestionnaire.afficherDetailsLivres();
         gestionnaire.afficherTitreEtAuteurLivres();
