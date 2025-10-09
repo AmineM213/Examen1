827714ccda8207a3d270c9a59caf6c1d17c2e617
yo <ledicodeyo@gmail.com>
2025-10-09 13:22:26 -0400

    1 ajout de laptop

--- src/main/java/cal/info/ApplicationStock.java
+++ src/main/java/cal/info/ApplicationStock.java
@@ -3,8 +3,7 @@ package cal.info;
 public class ApplicationStock {
     public static void main(String[] args) {
         GestionnaireStock gestionnaire = new GestionnaireStock();
-        gestionnaire.ajouterProduit(new Produit("Laptop", 5));
-        gestionnaire.ajouterProduit(new Produit("Laptop", 5));
+        gestionnaire.ajouterProduit(new Produit("Laptop", 10));
         gestionnaire.ajouterProduit(new Produit("Smartphone", 10));
         gestionnaire.ajouterProduit(new Produit("Tablette", 0));
         gestionnaire.afficherProduitsEnStock();
